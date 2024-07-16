967f7d7192df331cd311e41e6c37b24e5db808b5
Jaya0611 <161932606+Jaya0611@users.noreply.github.com>
2024-07-16 22:14:06 +0530

    Add files via upload

---
+++ assignment4-5.js
@@ -0,0 +1,6 @@
+// 5. Take 2 boolean variables as true and false and perform And and Or logical operation over them and print the 
+// result as well as operation 
+var a=true;
+var b=false;
+console.log(`logical &&: ${a&&b}`);
+console.log(`logical ||: ${a||b}`);
