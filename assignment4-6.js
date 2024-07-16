690a9cf4fe57075740f0694e42243e3c3f64729e
Jaya0611 <161932606+Jaya0611@users.noreply.github.com>
2024-07-16 22:14:31 +0530

    Add files via upload

---
+++ assignment4-6.js
@@ -0,0 +1,6 @@
+// 6. Take 2 variables and compare them using == and === such that result of both the comparisons operation 
+// is not equal."
+var num=2;
+var str="2";
+console.log(num==str);//true
+console.log(num===str);
