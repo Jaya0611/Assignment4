089aec1f6c6534b3cbf06315d44908cca7219a3f
Jaya0611 <161932606+Jaya0611@users.noreply.github.com>
2024-07-16 22:12:27 +0530

    Add files via upload

---
+++ assignment4-1.js
@@ -0,0 +1,9 @@
+/*1. Take two numbers and perform all the arithmetic operations over them. */
+var a=20;
+var b=10;
+console.log(`sum of ${a} and ${b} is: ${a+b}`);
+console.log(`sub of  ${a} and ${b} is: ${a-b}`);
+console.log(`mul of  ${a} and ${b} is: ${a*b}`);
+console.log(`div of  ${a} and ${b} is: ${a/b}`);
+console.log(`modulus of  ${a} and ${b} is: ${a%b}`);
+console.log(`a^b is: ${a**b}`);
