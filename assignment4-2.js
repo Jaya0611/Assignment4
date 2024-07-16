0ea4093e0fcf68e7f76cc201c583e2d50d7bec8c
Jaya0611 <161932606+Jaya0611@users.noreply.github.com>
2024-07-16 22:12:51 +0530

    Add files via upload

---
+++ assignment4-2.js
@@ -0,0 +1,4 @@
+// 2. Take 2 numbers and 2 strings and perform addition operation on them in same sequence. 
+var num1=2;
+var str1="2";
+console.log(`sum of ${num1} and ${str1} is: ${num1+str1}`);//22
